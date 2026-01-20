506d4fa048da21a3197dc52e0a72473524ecd68e
Anisur Rahman <anisoncloud@gmail.com>
2026-01-20 18:24:01 +0600

    using database

--- airflownext/app/myposts/[id]/page.js
+++ airflownext/app/myposts/[id]/page.js
@@ -3,15 +3,16 @@ import Link from "next/link";
 export default async function Page({ params }) {
   const { id } = await params;
 
-  const [rows] = await db.execute('select * from doctors where id = ?',[id]);
+  const [rows] = await db.execute('select * from posts where id = ?',[id]);
   const post = rows[0];
 
   return (
     <div>
       {post ? (
         <>
-          <p>{post.name}</p>
-          <p>{post.email}</p>
+          <p>{post.post_title}</p>
+          <p>{post.post_content}</p>
+          <p>{post.download_file}</p>
           <p>{post.created_at.toString()}</p>
           <p>{post.updated_at.toString()}</p>
         
--- airflownext/app/myposts/page.js
+++ airflownext/app/myposts/page.js
@@ -1,14 +1,14 @@
 import { db } from "@/config/db.js";
 import Link from "next/link";
 export default async function page() {
-    const [posts] = await db.execute('select * from doctors');
+    const [posts] = await db.execute('select * from posts');
     console.log(posts);
   return (
     <div>
         <ul className="list-group list-group-flush">
         {
         posts.map((item)=>{
-            return <li className="list-group-item" key={item.id}><Link href={`/myposts/${item.id}`} >{item.name}</Link></li> 
+            return <li className="list-group-item" key={item.id}><Link href={`/myposts/${item.id}`} >{item.post_title}</Link></li> 
         }) 
     }
     </ul>
